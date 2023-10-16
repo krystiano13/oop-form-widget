08e4ac9bed05d179b2e389a725d2765541c04f94
krystiano13 <crystianotv@gmail.com>
2023-10-16 16:22:49 +0200

    create Form class

---
+++ src/Form.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+class Form
+{
+    private string $action;
+    private string $method = "GET";
+    private string $content;
+
+    /**
+     * Elements array
+     * @var array<string>
+     */
+    private array $elements = [];
+
+    public function __construct(string $action, string $method)
+    {
+        $this->action = $action;
+        $this->method = $method;
+        $this->elements = [];
+    }
+
+    public function insert(string $element):void {
+        array_push($this->elements, $element);
+    }
+
+    public function render() {
+        foreach ($this->elements as $item) {
+            $this->content += $item;
+        }
+
+        return <<<FORM
+            <form>
+            {$this->content}
+            </form>
+        FORM;
+
+    }
+}
