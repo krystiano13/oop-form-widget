a3031619b3e56f7863ab0375123e4f329145f0de
krystiano13 <crystianotv@gmail.com>
2023-10-16 16:05:35 +0200

    create basic html

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Simple form build using OOP">
+        <title>OOP Form</title>
+        <meta name="author" content="Krystian Zieja" >
+        <meta name="keywords" content="oop, form, abstraction, php" >
+    </head>
+    <body>
+
+    </body>
+</html>
