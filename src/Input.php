164867f75b3f7a1d7163faa2f714dd02b539583c
krystiano13 <crystianotv@gmail.com>
2023-10-16 16:10:51 +0200

    create input class

---
+++ src/Input.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+class Input
+{
+    private string $type;
+    private string $placeholder;
+    private string $name;
+
+    public function __construct(string $type, string $placeholder, string $name)
+    {
+        $this->type = $type;
+        $this->placeholder = $placeholder;
+        $this->name = $name;
+
+    }
+
+    public function __invoke():string
+    {
+        return "<input type='{$this->type}' placeholder='{$this->placeholder}' name='{$this->name}' />";
+    }
+}
